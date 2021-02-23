383f87dbacc3784bf94427c8c5909c7ff08e41cf
lexx54 <chapelexx54@gmail.com>
2021-02-23 16:06:00 -0400

    message display wheter inputs need to be fill up or not

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const d=document,
   $plus=d.querySelectorAll(".plus"),
   $result=d.querySelectorAll(".amount"),
   $price=d.querySelectorAll(".price1"),
-  $shippingPrice=d.querySelector('.shippingPrice').innerText;
+  $shippingPrice=d.querySelector('.shippingPrice').innerText,
+  $inputs=d.querySelectorAll("input");
   let $totalPrice=d.querySelector(".totalPrice");
   let [amount1,amount2]= [parseInt($result[0].innerText),parseInt($result[1].innerText)];
 let [totalPrice1,totalPrice2,shippping]=[0,0,parseInt($shippingPrice)]
 
 
 d.addEventListener("click",e=>{
-  
   switch(e.target){
     case $minus[0]:
       amount1<=0? amount1=0 :amount1--;
@@ -25,14 +25,35 @@ d.addEventListener("click",e=>{
       amount2++;
     break;
   }
+   //update the values
   [totalPrice1,totalPrice2]=[
     parseFloat($price[0].innerText.split("$")[1])*amount1,
     parseFloat($price[1].innerText.split("$")[1])*amount2
   ];
-  //update the values
   $result[0].innerText=amount1;
   $result[1].innerText=amount2;
   $totalPrice.innerText=`${(totalPrice1+totalPrice2+shippping).toFixed(2)}$`;
 
-  // $totalPrice.innerText=(amount1*parseInt($price[0].innerText)+amount2*parseInt($price[1].innerText))+parseInt($shippingPrice.innerText)
+  if(e.target.matches(".btn")){
+    [...$inputs].every(ele=>ele.value!=="")
+    ?createMessage("success")
+    :createMessage("fail");
+  }
 })
+
+function createMessage(message){
+  const $head=d.createElement("div")
+  para=d.createElement("p"),
+  $body=d.body;
+  $head.classList.add(message);
+  
+  para.textContent=message=="success"? "Information sent":"Some Fields need to be Fill";
+  $head.appendChild(para);
+  $body.appendChild($head);
+
+  setTimeout(()=>{
+  $body.removeChild($head)
+  },1500)
+}
+
+
